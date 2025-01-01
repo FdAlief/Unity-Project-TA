a2affe8b228188c3718f24f4d20b3cef42c7f0e0
Fido Alief <fidoalief24@gmail.com>
2025-01-01 14:13:54 +0700

    Pembuatan Change Layer Biji
    Prefab Biji memiliki layer Default kemudian jika berada di Inventory layer akan menjadi Seed Layer agar bisa di drag dan ketika diluar Inventory menjadi Default Layer.
    
    Update Script InventoryManager :
    - Buat method ChangeSeedLayer

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -30,6 +30,9 @@ public class InventoryManager : MonoBehaviour
                 // Pindahkan prefab biji ke dalam slot
                 PlaceSeedInSlot(seed, inventorySlots[i]);
 
+                // Ubah layer (di dalam inventory)
+                ChangeSeedLayer(seed, "Seed Layer");
+
                 return true;
             }
         }
@@ -50,6 +53,9 @@ public class InventoryManager : MonoBehaviour
                 break;
             }
         }
+
+        // Ubah layer (di luar inventory)
+        ChangeSeedLayer(seed, "Default");
 
         // Pastikan biji tidak lagi menjadi child dari slot
         seed.transform.SetParent(null);
@@ -102,5 +108,14 @@ public class InventoryManager : MonoBehaviour
             }
         }
         return true; // Semua slot kosong
+    }
+
+    // Method untuk mengubah layer prefab biji
+    // Digunakan pada method AddSeedToInventory dan RemoveSeedFromInventory
+    private void ChangeSeedLayer(GameObject seed, string layerName)
+    {
+        // Ubah layer pada seed
+        seed.layer = LayerMask.NameToLayer(layerName);
+        Debug.Log("Layer biji diubah menjadi: " + layerName);
     }
 }
