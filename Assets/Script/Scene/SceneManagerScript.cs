236ccceb3aa1a6e7941cc3bfe8f5a1888d4af42b
Fido Alief <fidoalief24@gmail.com>
2025-02-10 11:41:42 +0700

    Build Test 1
    Semua Mekanik, terkecuali Biji Speisal

--- Assets/Script/Scene/SceneManagerScript.cs
+++ Assets/Script/Scene/SceneManagerScript.cs
@@ -5,6 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) // Tombol back di Android
+        {
+            // Jika ini bukan scene pertama, kembali ke scene sebelumnya
+            if (SceneManager.GetActiveScene().buildIndex > 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
+            else
+            {
+                // Jika sudah di scene pertama, bisa exit game
+                Application.Quit();
+            }
+        }
+    }
+
     // Method Pindah Scene
     public void ChangeScene(string scene)
     {
